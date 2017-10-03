9af623c3c0d79185fcccab5194d2891921a57b68
Martin-MacDonald <mmacdo54@caledonian.ac.uk>
2017-10-03 14:54:59 +0100

    Initial commiy

---
+++ src/GasStationSim.java
@@ -0,0 +1,19 @@
+package com.martin_bolton_macdonald;
+
+public class GasStationSim{
+	
+	public static void main(String[] args){
+		
+		//TODO: generate type of vehicle and create vehicle object with set volume of tank
+		//TODO: create new petrol tank and check if empty
+		//TODO: if empty ask to move to another tank
+		//TODO: Tell user how much petrol they have in their vehicle and ask if they want to put in a set amount of money or fill up?
+		//TODO: Start filling sequence
+		//TODO: fill up 1litre at a time
+		//TODO: stop if, reservoir is empty, car is full or money amount has been reached
+		//TODO: direct driver to pay amount or calculate amount first
+		//TODO: end
+		
+	}
+	
+}
